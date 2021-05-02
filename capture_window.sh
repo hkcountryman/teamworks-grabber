27562aa45ad1b547e8c8b7d094f11ecd27a39456
Halle Countryman <hkcountryman@gmail.com>
2021-05-01 18:06:30 -0700

    Script to get image works

---
+++ capture_window.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Bind this script to a key that can be pressed while Teamworks is open.
+# This means the script needs execute and read permissions (chmod u+r+x).
+# XFCE Settings Manager > Keyboard > Application Shortcuts
+# I mapped mine (~/GitHub/capture_window.sh) to super + print
+
+xfce4-screenshooter -ws ~/GitHub/teamworks-grabber/tmp_images/tmp.jpeg
