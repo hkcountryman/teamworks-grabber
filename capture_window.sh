30f40a7cc0498a08ef1e93930017c9e5c7e9a416
Halle Countryman <hkcountryman@gmail.com>
2021-05-01 18:17:06 -0700

    update script to create tmp_images folder

--- capture_window.sh
+++ capture_window.sh
@@ -3,6 +3,14 @@
 # Bind this script to a key that can be pressed while Teamworks is open.
 # This means the script needs execute and read permissions (chmod u+r+x).
 # XFCE Settings Manager > Keyboard > Application Shortcuts
-# I mapped mine (~/GitHub/capture_window.sh) to super + print
+# I mapped mine (~/GitHub/capture_window.sh) to super + print.
 
-xfce4-screenshooter -ws ~/GitHub/teamworks-grabber/tmp_images/tmp.jpeg
+# Check for directory to temporarily store screenshots:
+if [ ! -d "/home/halle/GitHub/teamworks-grabber/tmp_images" ]
+then
+    # Create it if absent:
+    mkdir /home/halle/GitHub/teamworks-grabber/tmp_images
+fi
+
+# Take a picture of the active window and save it in the temp folder:
+xfce4-screenshooter -ws ~/GitHub/teamworks-grabber/tmp_images/tmp.jpeg      # May want to use -r instead of -w to grab a region for easier parsing?
